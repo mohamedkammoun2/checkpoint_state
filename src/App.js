af0f01fd1fae13a86c6ba5b8965ed2aeca292629
mohamedkammoun2 <127191477+mohamedkammoun2@users.noreply.github.com>
2023-06-22 10:51:09 +0100

    checkpoint-state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
+
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, { Component } from 'react'
 
-export default App;
+export default class App extends Component {
+  state={
+    personne: {
+      fullName: "Mohamed kammoun",
+      bio: "Lorem ipsum dolor sit amet",
+      imgsrc:"https://img.freepik.com/photos-premium/portrait-homme-affaires-heureux-telephone-intelligent-au-bureau_637285-8763.jpg"
+      ,profession: "Développeur"
+    },
+  
+    isShow:false,
+    count:0,
+  }
+  handlePlus=()=>this.setState({count:this.state.count+1})
+  handleShow=()=>this.setState({isShow:!this.state.isShow})
+  
+  componentDidMount(){
+
+    setInterval(this.handlePlus, 1000);
+  }
+  
+  render() {
+    console.log(this.state)
+    return (
+      <div className="App">
+        <h1>{this.state.count}</h1>
+        <button onClick={this.handleShow}>Show</button>
+        { this.state.isShow?
+     <>
+      <h1>{this.state.personne.fullName}</h1> 
+      <h1>{this.state.personne.bio}</h1> 
+      <img src="https://img.freepik.com/photos-premium/portrait-homme-affaires-heureux-telephone-intelligent-au-bureau_637285-8763.jpg" alt="" />
+      <h1>{this.state.personne.profession}</h1> 
+     
+     </>
+      :null}
+      
+      </div>
+    )
+  }}
